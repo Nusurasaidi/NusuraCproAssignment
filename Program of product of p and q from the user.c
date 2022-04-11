5d58dd592b59ce7e6f2a0d7b79b677cbadc67939
Nusurasaidi <102525635+Nusurasaidi@users.noreply.github.com>
2022-04-11 17:39:02 +0300

    Add files via upload

---
+++ Program of product of p and q from the user.c
@@ -0,0 +1,19 @@
+//a program to be find the product of two numbers in from the user
+#include<atdio.h>
+int main()
+^{
+int p;
+int q;
+int product
+
+    printf("enter the first number\n");
+    scanf("%d",&p");
+    
+    printf("enter the second number\n");
+    scanf("%q",&q");
+    
+    product p*q
+    printf("the product of the two numbers from user is %d", product);
+
+    return 0;
+} 
---
+++ Program to print my name.c
@@ -0,0 +1,9 @@
+// a program to print Nusura Said
+#include<stdio.h>
+int main ()
+
+^{
+    printf("Nusura Said ");
+
+    return 0;
+}^
